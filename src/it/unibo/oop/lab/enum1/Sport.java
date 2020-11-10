250faaf237f3e67fc8e5c7e37f96d225b1ddd1a6
Giacomo <giacomo.totaro2@studio.unibo.it>
2020-11-10 17:35:40 +0100

    Finito es 3 --> enum1

--- src/it/unibo/oop/lab/enum1/Sport.java
+++ src/it/unibo/oop/lab/enum1/Sport.java
@@ -10,7 +10,28 @@ package it.unibo.oop.lab.enum1;
  * 
  */
 public enum Sport {
-    /*
+    BASKET("basket"),
+    SOCCER("soccer"),
+    TENNIS("tennis"),
+    BIKE("bike"),
+    F1("f1"),
+    MOTOGP("motogp"),
+    VOLLEY("volley");
+	
+	private final String Name;
+	
+	private Sport(final String name) {
+		this.Name = name;
+	}
+
+	public String getName() {
+		return Name;
+	}
+	
+	public String toString() {
+		return this.Name;
+	}
+	/*
      * declare the following sports: - basket - soccer - tennis - bike - F1 -
      * motogp - volley
      */
